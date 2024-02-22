e2528513684c65b10627c2b79ba871ed0af6a608
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2024-02-22 00:03:03 -0300

    desafio pronto

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import { BuscaClimaService } from '../../services/busca-clima.service';
+import { BuscaClimaService } from '../../services/busca-clima/busca-clima.service';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { BuscaClima } from '../../services/busca-clima/busca-clima.types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, ReactiveFormsModule],
+  imports: [HeaderComponent, ReactiveFormsModule, CommonModule],
   providers: [BuscaClimaService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   buscarClimaForm!: FormGroup;
+
+  buscaClimaResponse!: BuscaClima;
+
+  tipoTemperatura: 'CELSIUS' | 'FAHRENHEIT' = 'CELSIUS';
 
   constructor(private service: BuscaClimaService) {
     this.buscarClimaForm = new FormGroup({
@@ -21,8 +27,21 @@ export class HomeComponent {
   }
 
   onSubmit() {
-    console.log('estou auqi')
-    this.service.buscaClima(this.buscarClimaForm.value.cidade).subscribe(
-    );
+    this.service.buscaClima(this.buscarClimaForm.value.cidade).subscribe({
+      next: (data) => (this.buscaClimaResponse = data),
+      error: () => console.error('Erro ao tentar buscar a cidade'),
+    });
+  }
+
+  converterKelvinParaCelsius(kelvin: number): number {
+    return Number.parseInt(String(kelvin - 273.15));
+  }
+
+  converterKelvinParaFahrenheit(kelvin: number): number {
+    return Number.parseInt(String(((kelvin - 273.15) * 9) / 5 + 32));
+  }
+
+  ventoEmKmPorHora(speed: number): number {
+    return Math.round(speed * 3.6);
   }
 }
